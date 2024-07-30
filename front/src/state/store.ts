eb4501aaaf69c1d1b493d1dc7192382f60d626b7
Mbformation <immvp@hotmail.fr>
2024-07-31 00:34:05 +0200

    feat: :sparkles: Added store to manage state

---
+++ front/src/state/store.ts
@@ -0,0 +1,26 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "./user/userSlice";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
+const persistConfig = {
+  key: "user",
+  storage,
+  whitelist: ["token", "profile", "isSignedIn"], // persister uniquement 'token' et 'profile'
+};
+
+const persistedUserReducer = persistReducer(persistConfig, userReducer);
+
+export const store = configureStore({
+  reducer: {
+    user: persistedUserReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
