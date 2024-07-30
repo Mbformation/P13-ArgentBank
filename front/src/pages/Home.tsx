9e96ce1df1ed7cd176e2c200fe1da1d963e94140
Mbformation <immvp@hotmail.fr>
2024-07-31 00:22:48 +0200

    feat: :sparkles: Adding Home page component

---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,38 @@
+import Header from "../components/Header";
+import FeatureItem from "../components/FeatureItem";
+import Footer from "../components/Footer";
+
+export default function Home(){
+    //REACT ROUTER ==> récupérer donnée API
+    // Redux : State management
+    return (
+        <div>
+            <Header />
+            <main>
+                <div className="hero">
+                    <section className="hero-content">
+                        <h2 className="sr-only">Promoted Content</h2>
+                        <p className="subtitle">No fees.</p>
+                        <p className="subtitle">No minimum deposit.</p>
+                        <p className="subtitle">High interest rates.</p>
+                        <p className="text">Open a savings account with Argent Bank today!</p>
+                    </section>
+                </div>
+                <section className="features">
+                    <h2 className="sr-only">Features</h2>
+                    <FeatureItem 
+                    image={"./img/icon-chat.png"} 
+                    title={"You are our #1 priority"} 
+                    description={"Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."}/>
+                    <FeatureItem image={"./img/icon-money.png"} 
+                    title={"More savings means higher rates"} 
+                    description={"The more you save with us, the higher your interest rate will be!"}/>
+                    <FeatureItem image={"./img/icon-security.png"} 
+                    title={"Security you can trust"}
+                    description={"We use top of the line encryption to make sure your data and money is always safe."}/>
+                </section>
+            </main>
+            <Footer />
+        </div>
+    )
+}
